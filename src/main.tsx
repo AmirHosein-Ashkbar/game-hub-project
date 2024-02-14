863838afd69e8eaaad9368732e474e36c9af670c
AmirHosein Ashkbar <amir.h.ashkbar@gmail.com>
2024-02-14 08:41:31 +0330

    fetching genres

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,8 @@ import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import App from './App'
 import theme from './theme'
 import './index.css'
-import { QueryClient } from '@tanstack/react-query'  
-import { QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'  
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools' 
 
 const queryClient = new QueryClient(); 
 
@@ -15,6 +15,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <ChakraProvider theme={theme}>
         <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
         <App />
+        <ReactQueryDevtools/>
       </ChakraProvider>
     </QueryClientProvider>
   </React.StrictMode>,
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "100300a92f4e44aaa6a1b445ffad23f0",
   },
 });
