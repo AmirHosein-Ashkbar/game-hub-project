44630e43f92473d1c07f9c6afe3ad0475b9023fd
AmirHosein Ashkbar <amir.h.ashkbar@gmail.com>
2024-02-25 12:12:15 +0330

    Refactor - move entity interfaces

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,14 @@
-import {
-  Button,
-  Heading,
-  HStack,
-  Image,
-  List,
-  ListItem,
-  Spinner,
-  Text,
-} from "@chakra-ui/react";
-import useGenres, { Genre } from "../hooks/useGenres";
+import
+  {
+    Button,
+    Heading,
+    HStack,
+    Image,
+    List,
+    ListItem,
+    Spinner
+  } from "@chakra-ui/react";
+import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 import useGameQueryStore from "../store";
 
