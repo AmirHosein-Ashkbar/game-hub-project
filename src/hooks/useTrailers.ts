7f441595bedccb465b6945b5d506ba8f51baf1ed
AmirHosein Ashkbar <amir.h.ashkbar@gmail.com>
2024-02-25 22:07:45 +0330

    Build game trailer

---
+++ src/hooks/useTrailers.ts
@@ -0,0 +1,15 @@
+import { useQuery } from "@tanstack/react-query";
+import { Trailer } from "../entities/Trailer";
+import ApiClient from "../services/api-client";
+
+
+const useTrailer = (id: number) => {
+    const apiClient = new ApiClient<Trailer>(`/games/${id}/movies`);
+    return useQuery({
+        queryKey: ["trailers" , id],
+        queryFn: apiClient.getAll
+    });
+} 
+
+
+export default useTrailer;
